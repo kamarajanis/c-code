98740cdea1942e0d7f94af67e6b980134745a6f5
kamarajanis <kamarajkamarajanis@gmail.com>
2018-11-26 16:26:58 +0530

    sizefind

---
+++ sizefind.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	printf("%d",sizeof(char));
+	printf("%d",sizeof(int));
+	printf("%d",sizeof(float));
+	printf("%d",sizeof(double));
+	return 0;
+}
